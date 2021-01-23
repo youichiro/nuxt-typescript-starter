4797904f3ac4100b4a2335a2b651b13193197629
youichiro <cinnamon416@gmail.com>
2021-01-24 02:32:57 +0900

    init

---
+++ components/TestComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Name: {{ fullName }}
+    Message: {{ message }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+interface User {
+  firstName: string
+  lastName: string
+}
+
+@Component
+export default class TestComponent extends Vue {
+  @Prop({ type: Object, required: true }) readonly user!: User
+
+  message: string = 'This is a message'
+
+  get fullName (): string {
+    return `${this.user.firstName} ${this.user.lastName}`
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1>This is Test</h1>
+  <TestComponent :user="testUser" />
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TestComponent from "~/components/TestComponent.vue";
+
+@Component({
+  components: {
+    TestComponent,
+  },
+})
+export default class IndexPage extends Vue {
+  myname = {
+      firstName: "aki",
+      lastName: "kato"
+    }
+  get testUser(){
+    return this.myname
+  }
+}
+</script>
